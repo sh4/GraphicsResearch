ff3a8122902c99a77e12a2449532cf2523cd712c
sh4 <sh4@users.noreply.github.com>
2016-10-23 13:41:08 +0900

    ジョブステータス表示の不具合を修正

--- Server/view/admin_job.php
+++ Server/view/admin_job.php
@@ -78,7 +78,7 @@ foreach ($job->getUnits() as $unit):
 ?>
     <tr>
         <td>
-            <?php if ($judgedCount === $job->getMaxAssignments()): ?>
+            <?php if ($judgedCount === $job->getQuestions()): ?>
                 <span class="label label-pill label-success">COMPLETED</span>
             <?php elseif ($judgedCount > 0): ?>
                 <span class="label label-pill label-primary">JUDGING</span>
