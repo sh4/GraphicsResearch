a7559f33d7eae821586c46370f1b5187e9bd4f83
sh4 <sh4@users.noreply.github.com>
2016-11-19 23:40:38 +0900

    クラスオートローダーをエントリポイントに移動

--- Server/index.php
+++ Server/index.php
@@ -1,3 +1,7 @@
 <?php 
+set_include_path(get_include_path() . PATH_SEPARATOR . dirname(__FILE__)."/app/lib");
+spl_autoload_register(function ($className) {
+    include_once strtr(trim($className, "\\/"), "\\", "/").".php";
+});
 require_once dirname(__FILE__)."/app/router.php";
 Router::instance()->Execute();
